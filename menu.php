5afa2cef2f9cf0f163bc33a76d3ebe892a6b780b
draquel <draquel@webjynx.com>
2016-07-28 21:57:01 -0400

    Remove Existing Content
    Remove Existing Content and update for skeleton pages

--- menu.php
+++ menu.php
@@ -2,47 +2,12 @@
 	require_once("script/_php/lib.php");
 	$menu = "<ul class=\"nav navbar-nav\">
 	  <!--HOME-->
+	  <li><a href=\"/pg/index\" target=\"#content\">Home</a></li>
 	  <li class=\"dropdown\" data-toggle=\"collapse\" data-target=\".navbar-ex1-collapse\">
 		<a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"/pg/about/\" target=\"#content\">About<span class=\"caret\"></span></a>
 		<ul class=\"dropdown-menu\">
-		  <li><a href=\"/pg/about/principals\" target=\"#content\">The Principals</a></li>
-		  <li><a href=\"/pg/about/speak\" target=\"#content\">Speaking</a></li>
-		  <li><a href=\"/pg/about/consult\" target=\"#content\">Consulting</a></li>
+		  <li><a href=\"/pg/about/other\" target=\"#content\">Other</a></li>
 		</ul>
-	  </li>
-	  <li class=\"dropdown\" data-toggle=\"collapse\" data-target=\".navbar-ex1-collapse\">
-		<a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"/pg/data/\" target=\"#content\">The Data<span class=\"caret\"></span></a>
-		<ul class=\"dropdown-menu\">
-		  <li><a href=\"/pg/data/ii\" target=\"#content\">The Case for Inclusive Innovation</a></li>
-		  <li><a href=\"/pg/data/we\" target=\"#content\">Women's High-Growth Entrepreneurship</a></li>
-		  <li><a href=\"/pg/data/clst\" target=\"#content\">Commercializing Life Sciences &amp; Tech</a></li>
-		  <li><a href=\"/pg/data/id\" target=\"#content\">Impact Digest</a></li>
-		</ul>
-	  </li>
-	  <li class=\"dropdown\" data-toggle=\"collapse\" data-target=\".navbar-ex1-collapse\">
-	  	<a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"/pg/project/\" target=\"#content\">Our Projects<span class=\"caret\"></span></a>
-		<ul class=\"dropdown-menu\">
-		  <li><a href=\"/pg/project/mw\" target=\"#content\">Men's Work</a></li>
-		  <li><a href=\"/pg/project/lst\" target=\"#content\">Life Sciences &amp; Tech</a></li>
-		  <li><a href=\"/pg/project/atc\" target=\"#content\">Access to Capital</a></li>
-		  <!--<li><a href=\"/pg/project/he\" target=\"#content\">100 Entrepreneurs</a></li>
-		  <li><a href=\"/pg/project/astia\" target=\"#content\">ASTIA</a></li>
-		  <li><a href=\"/pg/project/nwbc\" target=\"#content\">NWBC</a></li>-->
-		</ul>
-	  </li>
-	  <li><a href=\"/pg/blog\" target=\"#content\">Blog</a></li>
-	  <li class=\"dropdown\" data-toggle=\"collapse\" data-target=\".navbar-ex1-collapse\">
-	  	<a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"/pg/media/\" target=\"#content\">Media<span class=\"caret\"></span></a>
-		  <ul class=\"dropdown-menu\">
-			<li><a href=\"/pg/media/inquiries\" target=\"#content\">Inquiries</a></li>
-			<li><a href=\"/pg/media/releases\" target=\"#content\">Releases</a></li>
-		  </ul>
-	  </li>
-	  <li class=\"dropdown\" data-toggle=\"collapse\" data-target=\".navbar-ex1-collapse\">
-	  	<a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"/pg/connect/\" target=\"#content\">Connect<!--<span class=\"caret\"></span>--></a>
-		  <!--<ul class=\"dropdown-menu\">
-			<li><a href=\"/pg/connect/calendar\" target=\"#content\">Calendar</a></li>
-		  </ul>-->
 	  </li>
 	</ul>";
 	
