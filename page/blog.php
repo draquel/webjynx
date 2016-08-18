ca6ca562b84dfe7b1429c2cec5f464b412f76e6e
draquel <draquel@webjynx.com>
2016-08-18 10:22:00 -0400

    Added Blog Meta and Category Link List output

--- page/blog.php
+++ page/blog.php
@@ -1,11 +1,19 @@
-<?php
-	$blog_css = file_get_contents("css/blog.css");
+<?php 
+	//Initialize Database Connection
+	$_SESSION['db'] = new Sql();
+	$_SESSION['db']->init("localhost","root","Ed17i0n!");
+	$_SESSION['db']->connect("DBObj");	
 	
-	$_SESSION['Blog'] = new Blog();
+	$_SESSION['Blog'] = new Blog(1);
+	$_SESSION['Blog']->dbRead($_SESSION['db']->con("DBObj"));
+	$blog = $_SESSION['Blog']->toArray();
+	$_SESSION['Blog']->load($_SESSION['db']->con("DBObj"));
 	
 	/*if(isset($_REQUEST['a'])){ echo "A - ".$_REQUEST['a']; }
 	if(isset($_REQUEST['c'])){ echo "C - ".$_REQUEST['c']; }
 	if(isset($_REQUEST['p'])){ echo "P - ".$_REQUEST['p']; }*/
+	
+	$blog_css = file_get_contents("css/blog.css");
 ?>
 
 <!-- Page Specific Styles -->
@@ -20,82 +28,37 @@
         <div class="row">
             <div class="col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
                 <div class="blog-header">
-                <h1 class="blog-title">The Bootstrap Blog</h1>
-                <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+                <h1 class="blog-title"><?php echo $blog['Title']; ?></h1>
+                <p class="lead blog-description"><?php echo $blog['Description']; ?></p>
                 </div>
                 <div class="col-sm-8 blog-main">
-                  <div class="blog-post">
-                    <h2 class="blog-post-title">Sample blog post</h2>
-                    <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
-        
-                    <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-                    <hr>
-                    <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-                    <blockquote>
-                      <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    </blockquote>
-                    <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                    <h3>Heading</h3>
-                    <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                    <h4>Sub-heading</h4>
-                    <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                    <pre><code>Example code block</code></pre>
-                    <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-                    <h4>Sub-heading</h4>
-                    <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                    <ul>
-                      <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                      <li>Donec id elit non mi porta gravida at eget metus.</li>
-                      <li>Nulla vitae elit libero, a pharetra augue.</li>
-                    </ul>
-                    <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-                    <ol>
-                      <li>Vestibulum id ligula porta felis euismod semper.</li>
-                      <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
-                      <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
-                    </ol>
-                    <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
-                  </div><!-- /.blog-post -->
-        
-                  <div class="blog-post">
-                    <h2 class="blog-post-title">Another blog post</h2>
-                    <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
-        
-                    <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-                    <blockquote>
-                      <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    </blockquote>
-                    <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                    <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                  </div><!-- /.blog-post -->
-        
-                  <div class="blog-post">
-                    <h2 class="blog-post-title">New feature</h2>
-                    <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
-        
-                    <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                    <ul>
-                      <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                      <li>Donec id elit non mi porta gravida at eget metus.</li>
-                      <li>Nulla vitae elit libero, a pharetra augue.</li>
-                    </ul>
-                    <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                    <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-                  </div><!-- /.blog-post -->
-        
+                  <?php
+				 	$posts = $_SESSION['Blog']->getPage(1);
+					$post = $posts->getFirstNode();
+					while($post != NULL){
+						$p = $post->readNode();
+						$a = $p->toArray();
+						$html = "<div class=\"blog-post\">
+							<h2 class=\"blog-post-title\">".$a['Title']."</h2>
+							<p class=\"blog-post-meta\">".$a['Created']." by <a href=\"#\">".$a['Author']."</a></p>
+							".$a['HTML']."
+						</div>";
+						 echo $html;
+						$post = $post->getNext();
+					}
+				  ?>
                   <nav>
                     <ul class="pager">
                       <li><a href="#">Previous</a></li>
                       <li><a href="#">Next</a></li>
                     </ul>
                   </nav>
-        
                 </div><!-- /.blog-main -->
         
                 <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
                   <div class="sidebar-module sidebar-module-inset">
                     <h4>About</h4>
-                    <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                    <p><?php echo $blog['Description']; ?></p>
                   </div>
                   <div class="sidebar-module">
                     <h4>Archives</h4>
@@ -117,10 +80,19 @@
                   <div class="sidebar-module">
                     <h4>Categories</h4>
                     <ol class="list-unstyled">
-                      <li><a href="#">Category 1</a></li>
+                    <?php
+						$cat = $_SESSION['Blog']->getCategories()->getFirstNode();
+						while($cat != NULL){
+							$c = $cat->readNode();
+							$a = $c->toArray();
+							echo "<li><a href=\"".$a['Code']."\">".$a['Definition']."</a></li>";
+							$cat = $cat->getNext();
+						}
+					?>
+                      <!--<li><a href="#">Category 1</a></li>
                       <li><a href="#">Category 2</a></li>
                       <li><a href="#">Category 3</a></li>
-                      <li><a href="#">Category 4</a></li>
+                      <li><a href="#">Category 4</a></li>-->
                     </ol>
                   </div>
                   <div class="sidebar-module">
