51681ee2e54031692323596b925a443f889ac24d
Dan Raquel <draquel@webjynx.com>
2016-08-22 16:11:03 -0400

    added rss functionality

---
+++ rss/index.php
@@ -0,0 +1,20 @@
+<?php
+	require_once("script/_php/lib.php");
+	include("script/_php/DBObj/dbobj.php");
+	session_start();
+	//Initialize Database Connection
+	$_SESSION['dbName'] = "DBObj";
+	$_SESSION['db'] = new Sql();
+	$_SESSION['db']->init("localhost","root","Ed17i0n!");
+	$_SESSION['db']->connect($_SESSION['dbName']);
+	
+	$_SESSION['Blog'] = new Blog(1);
+	$_SESSION['Blog']->dbRead($_SESSION['db']->con($_SESSION['dbName']));
+	$_SESSION['Blog']->load($_SESSION['db']->con($_SESSION['dbName']));
+	
+	header("Content-Type:application/xml");
+	
+	$domain = "http://dev.webjynx.com";
+	
+	echo $_SESSION['Blog']->rssGenFeed($domain,$_SERVER['PHP_SELF']);
+?>
