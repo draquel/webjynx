70e068b067f7aee24ed758a334a5e55dec806377
Dan Raquel <draquel@webjynx.com>
2016-08-28 15:56:34 -0400

    updated styles needed javascript to compensate for this page

--- page/sitemap.php
+++ page/sitemap.php
@@ -1,3 +1,11 @@
+<?php
+	
+?>
+<!-- Page Specific Styles -->
+	<style>	#pg .navbar-nav > li > a:hover{ background-color:transparent !important; text-decoration:underline; }</style>
+<!-- Preload CSS Images -->    
+    <img class="hidden" src="/img/stock_head1.svg" alt="Header img 1" />
+<!-- Page Content -->
 <div id="pg" class="container-fluid">
     <div class="row">
     	<div class="col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1">
@@ -6,3 +14,10 @@
         </div>
     </div>
 </div>
+<script type="text/javascript">
+	head.ready(function() {
+		$(document).ready(function(){
+			$("#pg .navbar-right").removeClass("navbar-right");
+		});
+	});
+</script>
